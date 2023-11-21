ae6ac2b76e7efd3c4a315af3088bd2609b66d25b
Lexi Mattick <lexi.mattick@gmail.com>
2023-11-21 17:22:24 -0500

    Add rudimentary error handling

--- src/index.js
+++ src/index.js
@@ -79,48 +79,53 @@ app.use(express.static('src/static'))
 app.use(router)
 
 router.get('/', async (req, res) => {
-	const linodeInfo = {
-		asn: LINODE_ASN,
-		network: await ktr.lookupAsn(LINODE_ASN)
-	}
-
-	const templates = readTemplates()
-	const [ beforeSplit, afterSplit ] = templates.page.split(TEMPLATE_SPLITS.tracerouteStream)
-
-	// Get user IP
-	let userIp = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '').split(',')[0].trim()
-	if (userIp === 'localhost' || userIp === '::1') userIp = '76.76.21.21' // kognise.dev
-	userIp = userIp.replace(/^::ffff:/, '')
-
-	// Globals for EJS renders
-	const pageGlobals = {
-		userIp,
-		serverHost: SERVER_HOST,
-		serverIp: SERVER_IP,
-		isoDate: new Date().toISOString(),
-		ktrVersion,
-		paragraphs: null
-	}
-
-	// Start trace
-	const trace = ktr.trace(userIp)
-
-	// Begin responding	to request
-	res.status(200)
-	res.contentType('text/html')
-	res.write(ejs.render(beforeSplit, { pageGlobals }))
-
-	// Stream traceroute updates
-	let lastStreamId = null
-	trace.on('update', (update) => {
-		const { streamId, html, isTraceDone } = renderTracerouteUpdate({ update, pageGlobals, templates, lastStreamId, linodeInfo })
-		res.write(html)
-		lastStreamId = streamId
-		if (isTraceDone) {
-			pageGlobals.paragraphs = generateText(update)
-			res.end(ejs.render(afterSplit, { pageGlobals }))
+	try {
+		const linodeInfo = {
+			asn: LINODE_ASN,
+			network: await ktr.lookupAsn(LINODE_ASN)
 		}
-	})
+
+		const templates = readTemplates()
+		const [ beforeSplit, afterSplit ] = templates.page.split(TEMPLATE_SPLITS.tracerouteStream)
+
+		// Get user IP
+		let userIp = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '').split(',')[0].trim()
+		if (userIp === 'localhost' || userIp === '::1') userIp = '76.76.21.21' // kognise.dev
+		userIp = userIp.replace(/^::ffff:/, '')
+
+		// Globals for EJS renders
+		const pageGlobals = {
+			userIp,
+			serverHost: SERVER_HOST,
+			serverIp: SERVER_IP,
+			isoDate: new Date().toISOString(),
+			ktrVersion,
+			paragraphs: null
+		}
+
+		// Start trace
+		const trace = ktr.trace(userIp)
+
+		// Begin responding	to request
+		res.status(200)
+		res.contentType('text/html')
+		res.write(ejs.render(beforeSplit, { pageGlobals }))
+
+		// Stream traceroute updates
+		let lastStreamId = null
+		trace.on('update', (update) => {
+			const { streamId, html, isTraceDone } = renderTracerouteUpdate({ update, pageGlobals, templates, lastStreamId, linodeInfo })
+			res.write(html)
+			lastStreamId = streamId
+			if (isTraceDone) {
+				pageGlobals.paragraphs = generateText(update)
+				res.end(ejs.render(afterSplit, { pageGlobals }))
+			}
+		})
+	} catch (error) {
+		console.error(error)
+		res.sendStatus(500)
+	}
 })
 
 console.log('starting up...')
